c99d9b0888e5829f4bbf41d36c9aaad5bb74a54d
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-05-15 12:59:43 +0200

    Updated code for dotnXSS

--- Lektion-05/dotnetXSS/Controllers/HomeController.cs
+++ Lektion-05/dotnetXSS/Controllers/HomeController.cs
@@ -6,6 +6,25 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        Response.Cookies.Append("TheSuperSecret", "Detta är hemligt!!!");
         return View();
+    }
+
+    [HttpGet()]
+    public IActionResult FindVehicle(string regNumber)
+    {
+        // Fejka att vi går till en databas och söker upp en bil med inskickat regnummer...
+        var model = $"The vehicle with registrationnumber <i>{regNumber}</i> is a Volvo V70 2014";
+
+        // Response.ContentType = "text/plain";
+        Response.ContentType = "text/html";
+        return Content(model);
+    }
+
+    [HttpGet()]
+    public IActionResult FindVehicleWithView(string regNumber)
+    {
+        var model = $"The vehicle with registrationnumber <i>{regNumber}</i> is a Volvo V70 2014";
+        return View("VehicleDetails", model);
     }
 }
